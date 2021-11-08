8ab5bbe874d44e6ba3e352bf5c6d2a4c9b23aefe
rencarHDH <76925718+rencarHDH@users.noreply.github.com>
2021-11-08 15:20:39 +0900

    Add : antd 디자인 수정

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,7 +4,7 @@ const commonPaths = require("./webpack/paths");
 module.exports = {
   components: "./src/components/**/*.{js,jsx,ts,tsx}",
   require: [
-    path.join(commonPaths.antdStylesFolder, "sass", "antdDatePicker.scss"),
+    path.join(commonPaths.antdStylesFolder, "sass", "antdDateRangePicker.scss"),
   ],
   webpackConfig: require('./webpack.config.js')
 };
